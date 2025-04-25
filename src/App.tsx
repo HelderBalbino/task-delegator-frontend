9edca0c03eec455898c72a13b3a93d1b1265dbdf
Helder Balbino <helderbalbino@googlemail.com>
2025-04-25 14:14:50 +0100

    chore: fix formatting and structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 function App() {
     return ( 
     <>
-        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="border-0 shadow-2xl shadow-gray-600 rounded-3xl p-4 sm:mx-auto sm:w-full sm:max-w-sm">
-            <div className="flex align-center justify-center">
-              <div>
-                <LuClipboardList size={50} />
-              </div>
-              <h1 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                Project Management
-              </h1>
-            </div>)
+         <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="border-0 shadow-2xl shadow-gray-600 rounded-3xl p-4 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="flex align-center justify-center">
+            <div>
+              <LuClipboardList size={50} />
             </div>
-            </div>
+            <h1 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+              Project Management
+            </h1>
+          </div>
+
+          <h2 className="mt-2 text-center text-sm/6 text-gray-600">
+            Input your credentials to login
+          </h2>
         
             </> );
 
