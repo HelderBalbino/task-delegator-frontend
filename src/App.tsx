a2d9def9cd29dfa2e0d334c1d95ac5dd4726e6bf
Helder Balbino <helderbalbino@googlemail.com>
2025-04-25 14:15:22 +0100

    chore: add email input form to App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,27 @@ function App() {
           <h2 className="mt-2 text-center text-sm/6 text-gray-600">
             Input your credentials to login
           </h2>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
         
             </> );
 
