d122d56b65b937e21d497ec68efc69e73dd2a944
Helder Balbino <helderbalbino@googlemail.com>
2025-04-25 14:11:50 +0100

    chore: update App component to include layout and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 function App() {
-    return ()
+    return ( 
+    <>
+        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="border-0 shadow-2xl shadow-gray-600 rounded-3xl p-4 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="flex align-center justify-center">
+              <div>
+                <LuClipboardList size={50} />
+              </div>
+              <h1 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                Project Management
+              </h1>
+            </div>)
+            </div>
+            </div>
+        
+            </> );
 
 
 
