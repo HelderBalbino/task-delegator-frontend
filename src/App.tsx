ca5765786a8fb501841f60078dc4df3b0f444d24
Helder Balbino <helderbalbino@googlemail.com>
2025-04-26 13:54:06 +0100

    chore: fix layout structure in App component for better alignment

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 		<>
 			<div className='flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
 				<div className='border-0 shadow-2xl shadow-gray-600 rounded-3xl p-4 sm:mx-auto sm:w-full sm:max-w-sm'>
-					<div className='flex align-center justify-center'>
-						<div>
+					<div className='flex flex-col align-center justify-center'>
+						<div className=''>
 							<LuClipboardList size={50} />
 						</div>
 						<h1 className='text-center text-2xl/9 font-bold tracking-tight text-gray-900'>
