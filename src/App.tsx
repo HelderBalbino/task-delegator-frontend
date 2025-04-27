24da13e347552f0664327ef20786982dba45f7a9
Helder Balbino <helderbalbino@googlemail.com>
2025-04-27 23:59:01 +0100

    chore: update password input placeholder for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ function App() {
 										type='password'
 										autoComplete='current-password'
 										required
-										placeholder='••••••••'
+										placeholder='*******'
 										className='block w-full rounded-md bg-white/10 px-3 py-2 text-base text-white placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm'
 									/>
 								</div>
