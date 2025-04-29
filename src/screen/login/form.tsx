2be060eab17d495557ff1968072e7b77e1ce517b
Helder Balbino <helderbalbino@googlemail.com>
2025-04-30 00:00:22 +0100

    feat: define FormProps interface for login form submission

--- src/screen/login/form.tsx
+++ src/screen/login/form.tsx
@@ -1,3 +1,10 @@
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { LuClipboardList } from 'react-icons/lu';
+
+interface FormProps {
+	onSubmit: (values: {
+		email: string;
+		password: string;
+	}) => Promise<void | { email?: string; password?: string }>;
+}
