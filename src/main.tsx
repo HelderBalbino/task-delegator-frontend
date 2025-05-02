a6cc8b4f45ac0f6f6a21e0da963a59765853e1f0
Helder Balbino <helderbalbino@googlemail.com>
2025-05-02 23:38:49 +0100

    feat: add ProtectedRoute component for route authentication

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,17 @@ import './index.css';
 import Login from './screen/login/Login';
 import Dashboard from './screen/dashboard/Dashboard';
 import React from 'react';
+
+// component to protect the routes
+// it checks if the user is authenticated by checking if the token exists in local storage
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+	const token = localStorage.getItem('token'); // verify if the token exists in local storage
+	if (!token) {
+		// if the token does not exist, redirect to the login page
+		// the replace prop is used to remove the current entry from the history stack
+		// this means that the user will not be able to go back to the protected page using the back button
+		// this is useful for security reasons, to prevent the user from accessing the protected page without authentication
+		return <Navigate to='/' replace />;
+	}
+	return children; // if the token exists, render the children
+};
