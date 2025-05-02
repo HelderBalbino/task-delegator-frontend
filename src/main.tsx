50090e26abf8510eff0083840202a451012d5b45
Helder Balbino <helderbalbino@googlemail.com>
2025-05-02 23:36:50 +0100

    fix: add missing import for React in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,4 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import Login from './screen/login/Login';
 import Dashboard from './screen/dashboard/Dashboard';
+import React from 'react';
