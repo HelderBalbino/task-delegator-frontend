48664adf72ee4b7ed1cd1e5602d9c221092bff51
Helder Balbino <helderbalbino@googlemail.com>
2025-05-02 23:35:53 +0100

    fix: add missing import for Dashboard component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,3 +2,4 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import Login from './screen/login/Login';
+import Dashboard from './screen/dashboard/Dashboard';
