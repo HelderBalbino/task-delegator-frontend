835dde59d16aa9c97ef6d6ab87fb551195341b79
Helder Balbino <helderbalbino@googlemail.com>
2025-05-06 23:21:32 +0100

    refactor: format handleRegister function for improved readability in Register component

--- src/screen/register/register.tsx
+++ src/screen/register/register.tsx
@@ -2,11 +2,27 @@ import Form from './form';
 import { register } from '../../services/userService';
 
 function Register() {
-    const handleRegister = async (values: {
-        name: string;
-        email: string;
-        password: string;
-        sector_name: string;
-        company_name: string;
-    }) => {     }
+	const handleRegister = async (values: {
+		name: string;
+		email: string;
+		password: string;
+		sector_name: string;
+		company_name: string;
+	}) => {
+		try {
+			const response = await register(values); // Chama a função de registro da API
+			if (response.data) {
+				console.log('Registro bem-sucedido:', response.data);
+				return; // Retorna sem erros
+			}
+		} catch (err) {
+			return { email: 'Erro ao registrar. Tente novamente.' }; // Retorna erros
+		}
+	};
 
+	return (
+		<>
+			<Form onSubmit={handleRegister} />
+		</>
+	);
+}
