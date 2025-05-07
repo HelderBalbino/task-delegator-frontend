1d172b2640d1fabbced847a51855475a4a14a2ef
Helder Balbino <helderbalbino@googlemail.com>
2025-05-07 23:55:38 +0100

    feat: initialize axios instance for task API with base URL and headers

--- src/service/taskService.ts
+++ src/service/taskService.ts
@@ -3,3 +3,10 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_BASE_URL;
 
 const token = localStorage.getItem('token');
+
+const api = axios.create({
+	baseURL: API_URL + 'tasks',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
