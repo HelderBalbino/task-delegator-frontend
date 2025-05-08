1b370aeb2a7af66abf9c60adbe62ff65fbaf794e
Helder Balbino <helderbalbino@googlemail.com>
2025-05-08 15:39:03 +0100

    feat: reorganize imports for task service functions in dashboard component

--- src/screen/dashboard/dashboard.tsx
+++ src/screen/dashboard/dashboard.tsx
@@ -1,3 +1,9 @@
 import { useEffect, useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'; // icons for the hamburger menu and close button
 import { Task } from '../../types/task';
+import {
+	getTasksByPage,
+	inProgressTask,
+	completeTask,
+	deleteTask,
+} from '../../services/taskService';
