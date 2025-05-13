1f99364d10af205d00a9ec8572e223e9c0719be9
Helder Balbino <helderbalbino@googlemail.com>
2025-05-13 14:24:29 +0100

    feat: add required validation for password field in AddUserModal

--- src/screen/dashboard/modal/addUserModal.tsx
+++ src/screen/dashboard/modal/addUserModal.tsx
@@ -15,5 +15,6 @@ const AddUserModal = ({ onClose }: AddUserModalProps) => {
 			.required('Email is required'),
 		password: Yup.string(),
 		.min(6, "Password must be at least 6 characters")
+		.required("Password is required"),
 	});
 };
