fd72ba9327d40c63f754ba428af59db02be320f5
Helder Balbino <helderbalbino@googlemail.com>
2025-05-13 14:07:38 +0100

    fix: correct comment formatting for AddUserModalProps interface

--- src/screen/dashboard/modal/addUserModal.tsx
+++ src/screen/dashboard/modal/addUserModal.tsx
@@ -2,6 +2,7 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { registerUser } from '../../../services/userService';
 
+// define the modal props
 interface AddUserModalProps {
-	onClose: () => void;
+	onClose: () => void; // function to close the modal
 }
