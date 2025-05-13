53cb82e11792556d19973ed0003e064e6dad25f1
Helder Balbino <helderbalbino@googlemail.com>
2025-05-13 14:06:43 +0100

    feat: define AddUserModalProps interface for modal component

--- src/screen/dashboard/modal/addUserModal.tsx
+++ src/screen/dashboard/modal/addUserModal.tsx
@@ -1,3 +1,7 @@
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { registerUser } from '../../../services/userService';
+
+interface AddUserModalProps {
+	onClose: () => void;
+}
